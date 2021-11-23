f619b7ab2afcd90e260fbdb6436836fae59816ae
Adrián Bernárdez <adrianbernardez3@gmail.com>
2021-11-23 20:07:37 +0100

    Primera entrega de documentacion

---
+++ producer/producerTFM/anomally_detector.py
@@ -0,0 +1,16 @@
+from logreduce import Classifier, Tokenizer, render_html
+
+clf = Classifier(
+    # A function to normalize filename, for example to remove dates or id
+    filename_to_modelname=lambda fn: fn,
+    # A function to ignore some file, for example configuration file
+    keep_file=lambda _: True,
+    # A function to process line
+    process_line=Tokenizer.process
+)
+
+clf.train(["/home/adrian/Desktop/TFM/Modelo/LOG_OK"])
+
+result = clf.process(["/home/adrian/Desktop/TFM/Modelo/LOG_ERROR"])
+with open("report.html", "w") as of:
+    of.write(render_html(result))
--- producer/producerTFM/script_datos.py
+++ producer/producerTFM/script_datos.py
@@ -6,41 +6,53 @@ import os
 
 fileNameOk = "log_success"
 fileNameError = "log_fail"
-numOk = 1
-numError = 1
 fileContent = ""
 flag = False
 flagError = False
+flagFind = False
 directoryNameOK = "/log_process/OK"
 directoryNameERROR = "/log_process/ERROR"
 
 # Se crea el directorio si no existe
-os.makedirs(sys.argv[1] + directoryNameOK, exist_ok=False)
-os.makedirs(sys.argv[1] + directoryNameERROR, exist_ok=False)
+os.makedirs(sys.argv[1] + directoryNameOK, exist_ok=True)
+os.makedirs(sys.argv[1] + directoryNameERROR, exist_ok=True)
+
+list_ids = []
 
 only_files = [f for f in os.listdir(sys.argv[1]) if isfile(join(sys.argv[1], f))]
+
 for file in only_files:
-    with open(sys.argv[1] + "/" + file) as file_in:
+    with open(sys.argv[1] + "/" + file, encoding='latin-1') as file_in:
         for line in file_in:
-            if flag:
-                fileContent = fileContent + "\n" + line
-                if "ERROR" in line:
-                    flagError = True
-                elif sys.argv[2] in line:
-                    if flagError:
-                        text_file = open(sys.argv[1] + directoryNameERROR + "/" + fileNameError + numError + ".log", "wt")
-                        n = text_file.write(fileContent)
-                        text_file.close()
-                        numError = numError + 1
+            if sys.argv[2] in line:
+                line_split = line.split(" | ")
+                list_ids.append(line_split[3].replace("[", "").replace("]", ""))
+
+for idOp in list_ids:
+    for file in only_files:
+        with open(sys.argv[1] + "/" + file, encoding='latin-1') as file_in:
+            for line in file_in:
+                if line.startswith("2021"):
+                    if idOp in line:
+                        fileContent = fileContent + line
+                        if "ERROR" in line:
+                            flagError = True
+                        flagFind = True
                     else:
-                        text_file = open(sys.argv[1] + directoryNameOK + "/" + fileNameOk + numOk + ".log", "wt")
-                        n = text_file.write(fileContent)
-                        text_file.close()
-                        numOk = numOk + 1
-                    flag = False
-                    flagError = False
-                    fileContent = ""
-            else:
-                if sys.argv[2] in line:
-                    fileContent = line
-                    flag = True
+                        flagFind = False
+                elif flagFind:
+                    fileContent = fileContent + line
+
+    if flagError:
+        text_file = open(sys.argv[1] + directoryNameERROR + "/"
+                         + fileNameError + idOp + ".log", "wt")
+        n = text_file.write(fileContent)
+        text_file.close()
+    else:
+        text_file = open(sys.argv[1] + directoryNameOK + "/" + fileNameOk + idOp+ ".log", "wt")
+        n = text_file.write(fileContent)
+        text_file.close()
+
+    flagError = False
+    fileContent = ""
+
